9fb99501464473c960af65faeb12a05da597ce94
chinnihoney <chinni.honey333@gmail.com>
2022-08-14 17:55:41 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    vector<int> v1={1,8,7,6};
+    for(auto n:v1)
+    {
+        cout<<n;
+    }
+    return 0;
+}
